7fac5a50bda4f6ba326f91f1d1091b345046bd4c
lipoyang <lipoyang1453@gmail.com>
2024-02-28 13:46:33 +0900

    modify Python code

--- fisheye_python/fisheye.py
+++ fisheye_python/fisheye.py
@@ -1,12 +1,7 @@
-# [メモ1]
-# 方法1: Image.getpixel, putpixel → 遅い
-# 方法2: Image.load               → 方法1よりはかなり速く面倒もない
-# 方法3: Image.getdata/putdata    → 方法2よりさらに速いが、少しだけ面倒
-# 方法4: Image.tobytes/frombytes  → 方法3よりさらに面倒だが、速度の向上見られず
-# → 方法1～4 の中では 方法3 がいちばんマシだが、 じゅうぶんな性能は得られず。
-
+from numba import jit          # 方法5
 from tkinter import *
 from PIL import Image, ImageTk
+import numpy as np             # 方法5
 import math
 import os
 import time #デバッグ用
@@ -16,8 +11,9 @@ dir_path = os.path.dirname(__file__)
 image_path = os.path.join(dir_path, "lena_std.bmp")
 src_img = Image.open(image_path)
 # src_data = src_img.load()    # 方法2
-src_data = src_img.getdata()   # 方法3
+# src_data = src_img.getdata() # 方法3
 # src_data = src_img.tobytes() # 方法4
+src_data = np.asarray(src_img) # 方法5
 
 W, H = src_img.size # 画像のサイズ
 RAD = int(W * 0.6)  # レンズの半径
@@ -26,29 +22,58 @@ D = int(RAD * 0.3)  # レンズの中心から投影面までの距離　(小さ
 # 処理後の画像
 dst_img = Image.new('RGB', (W, H))
 # dst_data = dst_img.load()           # 方法2
-dst_data = [None] * len(src_data)     # 方法3
+# dst_data = [None] * len(src_data)   # 方法3
 # dst_data = bytearray(len(src_data)) # 方法4
+dst_data = np.zeros((H, W, 3), dtype=np.uint8) # 方法5
 
 # 線形補間用のバッファ (高速化のためグローバル変数に)
-R = [[0, 0], [0, 0]]
-G = [[0, 0], [0, 0]]
-B = [[0, 0], [0, 0]]
+# R = [[0, 0], [0, 0]]
+# G = [[0, 0], [0, 0]]
+# B = [[0, 0], [0, 0]]
 
 # レンズの中心座標の初期値は中央
 x0 = W // 2
 y0 = H // 2
-
-# マウスイベント
-def mouse_pressed(event):
-    global x0, y0
-    x0, y0 = event.x, event.y
-    draw()
+x0_prev = 0
+y0_prev = 0
 
 # 描画
 def draw():
-    global dst_data
+    global dst_data, x0, y0, x0_prev, y0_prev
+
+    if x0 <  0: x0 = 0
+    if x0 >= W: x0 = W - 1
+    if y0 <  0: y0 = 0
+    if y0 >= H: y0 = H - 1
+
+    # レンズの中心座標が変化していなければ描画しない
+    if x0 == x0_prev and y0 == y0_prev:
+        return
+    x0_prev = x0
+    y0_prev = y0
 
     start_time = time.time()
+
+    draw_sub(x0, y0, dst_data)
+
+    # dst_img.putdata(dst_data)                                       # 方法3
+    # dst_img = Image.frombytes('RGB', src_img.size, bytes(dst_data)) # 方法4
+    dst_img = Image.fromarray(dst_data)                               # 方法5
+    img_tk = ImageTk.PhotoImage(dst_img)
+    label.config(image=img_tk)
+    label.image = img_tk
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"time: {elapsed_time} sec")
+
+# 描画サブルーチン
+@jit(nopython=True) # 方法5
+# def draw():
+def draw_sub(x0, y0, dst_data): # 方法5
+    # global dst_data
+    # dst_data = bytearray(len(src_data)) # 方法4
+    # dst_data = np.zeros((H, W, 3), dtype=np.uint8) # 方法5
 
     # 写像後の座標
     for Y in range(H):
@@ -75,37 +100,33 @@ def draw():
                     color_val = interpolation(x, y) # 元画像から線形補間で色を取得
                     # dst_img.putpixel((X, Y), color_val) # 方法1
                     # dst_data[X,Y] = color_val           # 方法2
-                    dst_data[Y_offset + X] = color_val    # 方法3
+                    # dst_data[Y_offset + X] = color_val  # 方法3
                 else:
                     # 画像の外側なら黒塗り
                     # dst_img.putpixel((X, Y), (0, 0, 0)) # 方法1
                     # dst_data[X,Y] = (0, 0, 0)           # 方法2
-                    dst_data[Y_offset + X] = (0, 0, 0)    # 方法3
-                    # color_val = (0, 0, 0)               # 方法4
+                    # dst_data[Y_offset + X] = (0, 0, 0)  # 方法3
+                    color_val = (0, 0, 0)                 # 方法4
             else:
                 # レンズの外側なら黒塗り
                 # dst_img.putpixel((X, Y), (0, 0, 0))     # 方法1
                 # dst_data[X,Y] = (0, 0, 0)               # 方法2
-                dst_data[Y_offset + X] = (0, 0, 0)        # 方法3
-                # color_val = (0, 0, 0)                   # 方法4
+                # dst_data[Y_offset + X] = (0, 0, 0)      # 方法3
+                color_val = (0, 0, 0)                     # 方法4
             # 方法4
-            # dst_data[index  ] = color_val[0]
-            # dst_data[index+1] = color_val[1]
-            # dst_data[index+2] = color_val[2]
-
-    dst_img.putdata(dst_data)                                         # 方法3
-    # dst_img = Image.frombytes('RGB', src_img.size, bytes(dst_data)) # 方法4
-    img_tk = ImageTk.PhotoImage(dst_img)
-    label.config(image=img_tk)
-    label.image = img_tk
-
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"time: {elapsed_time} sec")
+            #dst_data[index  ] = color_val[0]
+            #dst_data[index+1] = color_val[1]
+            #dst_data[index+2] = color_val[2]
+            # 方法5
+            dst_data[Y, X] = color_val
 
 # 線形補間
+@jit(nopython=True) # 方法5
 def interpolation(x, y):
-    global R, G, B
+    # global R, G, B
+    R = np.zeros((2, 2))
+    G = np.zeros((2, 2))
+    B = np.zeros((2, 2))
 
     X = int(x)
     Y = int(y)
@@ -117,27 +138,45 @@ def interpolation(x, y):
             if _y >= H: _y = Y
             # R[i][j], G[i][j], B[i][j] = src_img.getpixel((_x, _y)) # 方法1
             # R[i][j], G[i][j], B[i][j] = src_data[_x, _y]           # 方法2
-            R[i][j], G[i][j], B[i][j] = src_data[_y * W + _x]        # 方法3
+            # R[i][j], G[i][j], B[i][j] = src_data[_y * W + _x]      # 方法3
             # 方法4
-            # index = (_y * W + _x) * 3
-            # R[i][j] = src_data[index]
-            # G[i][j] = src_data[index + 1]
-            # B[i][j] = src_data[index + 2]
+            #index = (_y * W + _x) * 3
+            #R[i][j] = src_data[index]
+            #G[i][j] = src_data[index + 1]
+            #B[i][j] = src_data[index + 2]
+            # 方法5
+            R[i, j] = src_data[_y, _x, 0]
+            G[i, j] = src_data[_y, _x, 1]
+            B[i, j] = src_data[_y, _x, 2]            
     dX = x - X
     dY = y - Y
     MdX = 1 - dX
     MdY = 1 - dY
-    r = round(MdX * (MdY * R[0][0] + dY * R[0][1]) + dX * (MdY * R[1][0] + dY * R[1][1]))
-    g = round(MdX * (MdY * G[0][0] + dY * G[0][1]) + dX * (MdY * G[1][0] + dY * G[1][1]))
-    b = round(MdX * (MdY * B[0][0] + dY * B[0][1]) + dX * (MdY * B[1][0] + dY * B[1][1]))
+    r = round(MdX * (MdY * R[0, 0] + dY * R[0, 1]) + dX * (MdY * R[1, 0] + dY * R[1, 1]))
+    g = round(MdX * (MdY * G[0, 0] + dY * G[0, 1]) + dX * (MdY * G[1, 0] + dY * G[1, 1]))
+    b = round(MdX * (MdY * B[0, 0] + dY * B[0, 1]) + dX * (MdY * B[1, 0] + dY * B[1, 1]))
     return r, g, b
+
+# マウスイベント
+def mouse_updown(event):
+    global x0, y0
+    x0, y0 = event.x, event.y
+    draw()
+def mouse_move(event):
+    global x0, y0
+    button_state = event.state
+    if button_state & 0x100:  # 左ボタン状態
+        x0, y0 = event.x, event.y
+        draw()
 
 # Tkinterウィンドウ
 root = Tk()
 root.title("Image Distortion")
 label = Label(root) # 画像を表示するラベル
 label.pack()
-root.bind("<Button-1>", mouse_pressed) # マウスクリックイベント
+root.bind("<Button-1>",        mouse_updown)
+root.bind('<ButtonRelease-1>', mouse_updown)
+root.bind('<Motion>',          mouse_move)
 
 draw()
 root.mainloop()
