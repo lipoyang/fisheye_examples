93d363bebb07ce8e84d722f59d8cd1270fc9eedb
lipoyang <lipoyang1453@gmail.com>
2024-02-28 13:32:20 +0900

    modify Python code

--- fisheye_python/fisheye.py
+++ fisheye_python/fisheye.py
@@ -1,3 +1,10 @@
+# [メモ1]
+# 方法1: Image.getpixel, putpixel → 遅い
+# 方法2: Image.load               → 方法1よりはかなり速く面倒もない
+# 方法3: Image.getdata/putdata    → 方法2よりさらに速いが、少しだけ面倒
+# 方法4: Image.tobytes/frombytes  → 方法3よりさらに面倒だが、速度の向上見られず
+# → 方法1～4 の中では 方法3 がいちばんマシだが、 じゅうぶんな性能は得られず。
+
 from tkinter import *
 from PIL import Image, ImageTk
 import math
@@ -9,8 +16,8 @@ dir_path = os.path.dirname(__file__)
 image_path = os.path.join(dir_path, "lena_std.bmp")
 src_img = Image.open(image_path)
 # src_data = src_img.load()    # 方法2
-# src_data = src_img.getdata() # 方法3
-src_data = src_img.tobytes()   # 方法4
+src_data = src_img.getdata()   # 方法3
+# src_data = src_img.tobytes() # 方法4
 
 W, H = src_img.size # 画像のサイズ
 RAD = int(W * 0.6)  # レンズの半径
@@ -18,9 +25,9 @@ D = int(RAD * 0.3)  # レンズの中心から投影面までの距離　(小さ
 
 # 処理後の画像
 dst_img = Image.new('RGB', (W, H))
-# dst_data = dst_img.load()         # 方法2
-# dst_data = [None] * len(src_data) # 方法3
-dst_data = bytearray(len(src_data)) # 方法4
+# dst_data = dst_img.load()           # 方法2
+dst_data = [None] * len(src_data)     # 方法3
+# dst_data = bytearray(len(src_data)) # 方法4
 
 # 線形補間用のバッファ (高速化のためグローバル変数に)
 R = [[0, 0], [0, 0]]
@@ -47,7 +54,8 @@ def draw():
     for Y in range(H):
         Y_offset = Y * W # 方法3,方法4
         for X in range(W):
-            index = (Y_offset + X) * 3 # 方法4
+            # index = (Y_offset + X) * 3 # 方法4
+
             # レンズの中心からの相対座標
             dX = X - x0
             dY = Y - y0
@@ -67,26 +75,26 @@ def draw():
                     color_val = interpolation(x, y) # 元画像から線形補間で色を取得
                     # dst_img.putpixel((X, Y), color_val) # 方法1
                     # dst_data[X,Y] = color_val           # 方法2
-                    # dst_data[Y_offset + X] = color_val  # 方法3
+                    dst_data[Y_offset + X] = color_val    # 方法3
                 else:
                     # 画像の外側なら黒塗り
                     # dst_img.putpixel((X, Y), (0, 0, 0)) # 方法1
                     # dst_data[X,Y] = (0, 0, 0)           # 方法2
-                    # dst_data[Y_offset + X] = (0, 0, 0)  # 方法3
-                    color_val = (0, 0, 0)                 # 方法4
+                    dst_data[Y_offset + X] = (0, 0, 0)    # 方法3
+                    # color_val = (0, 0, 0)               # 方法4
             else:
                 # レンズの外側なら黒塗り
                 # dst_img.putpixel((X, Y), (0, 0, 0))     # 方法1
                 # dst_data[X,Y] = (0, 0, 0)               # 方法2
-                # dst_data[Y_offset + X] = (0, 0, 0)      # 方法3
-                color_val = (0, 0, 0)                     # 方法4
+                dst_data[Y_offset + X] = (0, 0, 0)        # 方法3
+                # color_val = (0, 0, 0)                   # 方法4
             # 方法4
-            dst_data[index  ] = color_val[0]
-            dst_data[index+1] = color_val[1]
-            dst_data[index+2] = color_val[2]
+            # dst_data[index  ] = color_val[0]
+            # dst_data[index+1] = color_val[1]
+            # dst_data[index+2] = color_val[2]
 
-    # dst_img.putdata(dst_data) # 方法3
-    dst_img = Image.frombytes('RGB', src_img.size, bytes(dst_data)) # 方法4
+    dst_img.putdata(dst_data)                                         # 方法3
+    # dst_img = Image.frombytes('RGB', src_img.size, bytes(dst_data)) # 方法4
     img_tk = ImageTk.PhotoImage(dst_img)
     label.config(image=img_tk)
     label.image = img_tk
@@ -109,12 +117,12 @@ def interpolation(x, y):
             if _y >= H: _y = Y
             # R[i][j], G[i][j], B[i][j] = src_img.getpixel((_x, _y)) # 方法1
             # R[i][j], G[i][j], B[i][j] = src_data[_x, _y]           # 方法2
-            # R[i][j], G[i][j], B[i][j] = src_data[_y * W + _x]      # 方法3
+            R[i][j], G[i][j], B[i][j] = src_data[_y * W + _x]        # 方法3
             # 方法4
-            index = (_y * W + _x) * 3
-            R[i][j] = src_data[index]
-            G[i][j] = src_data[index + 1]
-            B[i][j] = src_data[index + 2]
+            # index = (_y * W + _x) * 3
+            # R[i][j] = src_data[index]
+            # G[i][j] = src_data[index + 1]
+            # B[i][j] = src_data[index + 2]
     dX = x - X
     dY = y - Y
     MdX = 1 - dX
