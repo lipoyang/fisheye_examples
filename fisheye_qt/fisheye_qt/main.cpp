0d84d9f0ef95b5179748e313461af81a63e349dc
lipoyang <lipoyang1453@gmail.com>
2024-03-03 11:47:06 +0900

    fix C++ code

--- fisheye_qt/fisheye_qt/main.cpp
+++ fisheye_qt/fisheye_qt/main.cpp
@@ -4,6 +4,15 @@
 
 int main(int argc, char *argv[])
 {
+    // 高DPIモニタだとなぜか表示が更新されない。Qt6のバグか？
+    // 暫定措置として、スケーリングを無効化する。
+#ifdef Q_OS_WINDOWS
+    qputenv("QT_ENABLE_HIGHDPI_SCALING", "0");
+#endif
+
+    // このAPIはQt6では廃止
+    // QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
