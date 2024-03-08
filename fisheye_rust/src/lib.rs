7972435c4dfe42c1068b5725af927ca2eced0495
lipoyang <lipoyang1453@gmail.com>
2024-03-08 17:04:31 +0900

    modify Rust code

--- fisheye_rust/src/lib.rs
+++ fisheye_rust/src/lib.rs
@@ -34,20 +34,19 @@ static mut SRC_DATA: Vec<u8> = Vec::new();
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue>
 {
-    console::log_1(&JsValue::from_str("Hello world!")); // TODO
+//  console::log_1(&JsValue::from_str("Hello world!"));
 
     // アプリケーションの変数を初期化
     let app:App = Default::default();
     let app = Rc::new(Cell::new(app));
 
-    // 表示用キャンバスを取得
+    // 表示用キャンバスとそのコンテキストを取得
     let document = web_sys::window().unwrap().document().unwrap();
     let dst_canvas = document.get_element_by_id("dstCanvas").unwrap();
     let dst_canvas = dst_canvas
         .dyn_into::<HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
-    // 表示用コンテキストを取得
     let dst_context = dst_canvas
         .get_context("2d")
         .unwrap()
@@ -118,24 +117,47 @@ pub fn start() -> Result<(), JsValue>
         let _src_image = src_image.clone();
         let app = app.clone();
         let dst_context = dst_context.clone();
+        
+        // 画像ファイル読み込み完了時の処理
         let closure = Closure::once_into_js(move |_event: web_sys::Event| {
-            // 画像ファイル読み込み完了時の処理
+            // 画像データをimg要素として取得
             let src_image = Rc::try_unwrap(_src_image).unwrap().into_inner(); // TODO
+
+            // 画面のサイズを取得
+            let document = web_sys::window().unwrap().document().unwrap();
+            let element = document.document_element().unwrap();
+            let client_width = element.client_width();
+            let client_height = element.client_height();
             
-            // アプリケーションの変数を設定
             let mut _app = app.get();
+
+            // 画像サイズ
             _app.w = src_image.width();
             _app.h = src_image.height();
             _app.r = _app.w as f64 * 0.6;
             _app.d = _app.r * 0.3; // 小さいほど大きく歪む
-            _app.x0 = (_app.w / 2) as i32;
-            _app.y0 = (_app.h / 2) as i32;
+
+            // 表示サイズ (画像サイズより画面が小さい場合があるため)
+            _app.w2 = _app.w;
+            _app.h2 = _app.h;
+            _app.mag = 1.0;
+            let c_w = ((client_width  as f64) * 0.96) as u32;
+            let c_h = ((client_height as f64) * 0.96) as u32;
+            let c_l = if c_w < c_h {c_w} else {c_h};
+            if c_l < _app.w {
+              _app.w2 = c_l;
+              _app.h2 = c_l;
+              _app.mag = (_app.w as f64) / (_app.w2 as f64);
+            }
+            // レンズの中心座標の初期値は中央
+            _app.x0 = (_app.w2 / 2) as i32;
+            _app.y0 = (_app.h2 / 2) as i32;
             _app.x0_prev = 0;
             _app.y0_prev = 0;
+
             app.set(_app);
             
             // 元画像のイメージデータを取得
-            let document = web_sys::window().unwrap().document().unwrap();
             let src_canvas = document.create_element("canvas")
                 .unwrap().dyn_into::<HtmlCanvasElement>().unwrap();
             let src_context = src_canvas.get_context("2d")
@@ -152,16 +174,15 @@ pub fn start() -> Result<(), JsValue>
             }
            
             // 表示画像のキャンバスサイズ設定
-            dst_canvas.set_width(_app.w);
-            dst_canvas.set_height(_app.h);
+            dst_canvas.set_width(_app.w2);
+            dst_canvas.set_height(_app.h2);
           
             // 描画
             draw(&mut _app, &dst_context);
-    
         }); // TODO
-        // TODO
         src_image.borrow_mut().set_onload(Some(closure.as_ref().unchecked_ref()));
         src_image.borrow_mut().set_src("./lena_std.bmp");
+        // TODO
     }
     Ok(())
 }
@@ -169,20 +190,23 @@ pub fn start() -> Result<(), JsValue>
 // 描画
 fn draw(_app: &mut App,dst_context: &CanvasRenderingContext2d)
 {
-    let size = _app.h * _app.w * 4;
+    let size = _app.h2 * _app.w2 * 4;
     let mut dst_data: Vec<u8> = vec![0; size as usize];
     
     let w = _app.w as i32;
     let h = _app.h as i32;
     let r = _app.r;
     let d = _app.d;
+    let w2 = _app.w2 as i32;
+    let h2 = _app.h2 as i32;
+    let mag = _app.mag;
     let mut x0 = _app.x0;
     let mut y0 = _app.y0;
 
-    if x0 <  0 { x0 = 0; } 
-    if x0 >= w { x0 = w - 1 };
-    if y0 <  0 { y0 = 0; }
-    if y0 >= h { y0 = h - 1 };
+    if x0 <  0  { x0 = 0; } 
+    if x0 >= w2 { x0 = w2 - 1 };
+    if y0 <  0  { y0 = 0; }
+    if y0 >= h2 { y0 = h2 - 1 };
     
     // レンズの中心座標が変化していなければ描画しない
     if (x0 == _app.x0_prev) && (y0 == _app.y0_prev) {
@@ -197,13 +221,13 @@ fn draw(_app: &mut App,dst_context: &CanvasRenderingContext2d)
     let start_time = js_sys::Date::now();
 
     // 写像後の座標
-    for y in 0..h {
-        for x in 0..w {
+    for y in 0..h2 {
+        for x in 0..w2 {
             let c;
             
             // レンズの中心からの相対座標
-            let dx = (x - x0) as f64;
-            let dy = (y - y0) as f64;
+            let dx = ((x - x0) as f64) * mag;
+            let dy = ((y - y0) as f64) * mag;
             let _d = (dx*dx + dy*dy).sqrt();
             if _d < r {
                 // 写像:元画像→魚眼画像
@@ -213,8 +237,8 @@ fn draw(_app: &mut App,dst_context: &CanvasRenderingContext2d)
                 // x = D*X/√(R^2-X^2-Y^2)
                 // y = D*Y/√(R^2-X^2-Y^2)
                 let z = (r*r - dx*dx - dy*dy).sqrt();
-                let _x = (x0 as f64) + (d * dx) / z;
-                let _y = (y0 as f64) + (d * dy) / z;
+                let _x = (x0 as f64) * mag + (d * dx) / z;
+                let _y = (y0 as f64) * mag + (d * dy) / z;
                 
                 if (_x >= 0.0) && (_x < w as f64) && (_y >= 0.0) && (_y < h as f64) {
                     c = interpolation(w as usize, h as usize, _x, _y); // 元画像から線形補間で色を取得
@@ -224,13 +248,13 @@ fn draw(_app: &mut App,dst_context: &CanvasRenderingContext2d)
             }else{
                 c = BLACK; // レンズの外側なら黒塗り
             }
-            let index = ((y * w + x) * 4) as usize;
+            let index = ((y * w2 + x) * 4) as usize;
             for i in 0..4 { dst_data[index + i] = c[i] };
         }
     }
 
     let dst_data = Clamped(&dst_data[..]);
-    let dst_data = ImageData::new_with_u8_clamped_array_and_sh(dst_data, _app.w, _app.h).unwrap();
+    let dst_data = ImageData::new_with_u8_clamped_array_and_sh(dst_data, _app.w2, _app.h2).unwrap();
     dst_context.put_image_data(&dst_data, 0.0, 0.0).unwrap();
 
     let end_time = js_sys::Date::now();
